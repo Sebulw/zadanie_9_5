9724c3c3019512af5f13468643f93b570dbe2f6b
Sebastian Wojnicz <sebulw@gmail.com>
2017-09-28 15:15:03 +0200

    script finished

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,8 +1,4 @@
-var navigation = document.getElementById('menu');
 var buttonClass = document.getElementsByClassName('button');
 for (var i = 0; i < buttonClass.length; i++) {
-	var newP = document.createElement('p');
-		newP.innerHTML = buttonClass;
-		navigation.appendChild(newP);
-
+	alert(buttonClass[i].innerText)
 }
